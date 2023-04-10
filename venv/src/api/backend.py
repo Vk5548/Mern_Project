526ecdf2fc9b2bb1b0e1149209c6ecaa88792e4f
Dharmendra Nasit <98192652+dharam51@users.noreply.github.com>
2023-04-09 21:26:41 -0400

    read POST data + mongodb queries

--- venv/src/api/backend.py
+++ venv/src/api/backend.py
@@ -1,6 +1,12 @@
+import pprint
 from flask import Flask
 from pymongo import MongoClient
 db = MongoClient().Vehicles
+from bson.son import SON
+from bson.objectid import ObjectId
+import gridfs
+from flask import request
+import json
 
 app = Flask(__name__)
 
@@ -11,7 +17,26 @@ def index():
 @app.route('/globalSearch', methods=['POST'])
 def global_search():
     #read string from frontend ["search string" either `county` or `electric vehicle type`]
+    data = json.load(request.get_json())
+    county = data['county']
+    evt = data['evt']
+
+    
     #mongo regex query
+    query = {
+        'County':
+            {
+                '$regex': county+'* '
+            },
+        'ElectricVehicleType':
+            {
+                '$regex': evt+'*'
+            }   
+    }
+    result = db.electric.find(query)
+    for each_record in result:
+        pprint.pprint(each_record)
+
 
     #return array of records
     return "global search"
@@ -19,21 +44,67 @@ def global_search():
 @app.route('/globalLocationSearch', methods=['POST'])
 def global_search():
     #read string from frontend [ "lat,long" , "within"]
+    data = json.load(request.get_json())
+    latitude = data['latitude']
+    longitude = data['longitude']
+    range = data['range']
+
+
     #mongo near query
+    query = {"location": SON([("$near", [longitude, latitude]), ("$maxDistance", range)])}
+    result = db.Geoelectric.find(query)
+    for each_record in result:
+        pprint.pprint(each_record)
+
     #return array of records
     return "global location search"
 
 @app.route('/filteredSearch', methods=['POST'])
 def global_search():
     #read string from frontend [ "_id "]
+    data = json.load(request.get_json())
+    id = data['id']
+    
+    
     #mongo `_id` query in both (Geo and non geo and comments) collection along with images & comments.
+    # READ IMAGE
+    fs = gridfs.GridFS(db)
+    metadata = db.electricimg.find_one({'electricid': id})
+    if len(metadata) == 0:
+        metadata = db.electricimg.find_one({'geoelectricid': id})
+    grid_out = fs.get(metadata['file_id'])
+    image_content = grid_out.read()
+
+    # READ TEXT
+    query = {"electricid": id}
+    result = db.electric.find_one(query)
+    print(result)
+
+    query = {"geoelectricid": id}
+    result = db.Geoelectric.find_one(query)
+    print(result)
+
+
     #return text + image + comments
     return "filtered search"
 
 @app.route('/addComments', methods=['POST'])
 def global_search():
     #read string from frontend [ "_id "]
+    data = json.load(request.get_json())
+    id = data['id']
+    comment = data['comments']
+    
     #mongo add in existing collection either add or update.
+    record = [{"comments":comment}]
+    query = {"electricid": id}, {"$push": {"annotations": record}}
+    result = db.electric.update_one(query, upsert=True)
+    print(result)
+
+    query = {"geoelectricid": id}, {"$push": {"annotations": record}}
+    result = db.Geoelectric.update_one(query, upsert=True)
+    print(result)
+
     #return boolean
     return "add comments True/ False"
 
